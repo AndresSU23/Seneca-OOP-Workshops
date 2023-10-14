c59261878a39b3754bb4942fa4dd3150773b3d62
AndresSU23 <111714730+AndresSU23@users.noreply.github.com>
2023-10-13 23:53:45 -0400

    W5 Lab
    submited to matrix

--- WS05/lab/Flight.cpp
+++ WS05/lab/Flight.cpp
@@ -5,7 +5,14 @@
 // Version 1.0
 // Author   Asam Gulaid, revised by Fardad Soleimanloo
 // Description
+//			Name      : David Andres Sanchez Umbarila
+//			Student ID: 140273228
+//			Email     : dasanchez-umbarila@myseneca.ca
+/			Date      : 07/10/2023
 //
+//			I have done all the coding by myself and only copied the code
+//			 that my professor provided to complete my workshops and
+//			 assignments.
 // Revision History
 // -----------------------------------------------------------
 // Name                 Date            Reason
@@ -78,6 +85,101 @@ namespace sdds {
             cout << " Liters";
         }
         return cout;
+    }
+
+    Flight::operator bool() const {
+        return (m_passengers > 0) && (m_fuel >= (m_passengers * LITER_PER_PERSON));
+    }
+
+    Flight::operator int() const {
+        return m_passengers;
+    }
+
+    Flight::operator double() const {
+        return m_fuel;
+    }
+
+    Flight::operator const char*() const {
+        return m_title;
+    }
+
+    bool Flight::operator~() const
+    {
+        return m_passengers == 0;
+    }
+
+    Flight& Flight::operator=(Flight& transferFlight) {
+        m_fuel = transferFlight.m_fuel;
+        m_passengers = transferFlight.m_passengers;
+        strcpy(m_title, transferFlight.m_title);
+        transferFlight.m_fuel = 0;
+        transferFlight.m_passengers = 0;
+        strcpy(transferFlight.m_title, "EmptyPlane");
+        return *this;
+    }
+
+    Flight& Flight::operator=(int passengers)
+    {
+        m_passengers = (passengers > 0 && m_passengers <= Boen747Capacity) ? passengers : m_passengers;
+        return *this;
+    }
+
+    Flight& Flight::operator=(double fuel)
+    {
+        m_fuel = (fuel > 0 && m_fuel <= FuelTankCapacity) ? fuel : m_fuel;
+        return *this;
+    }
+
+    Flight& Flight::operator+=(double addFuel)
+    {
+        m_fuel += (addFuel < 0 || ((m_fuel + addFuel) > FuelTankCapacity)) ? 0 : addFuel;
+        return *this;
+    }
+
+    Flight& Flight::operator+=(int addPasssengers)
+    {
+        m_passengers += (addPasssengers < 0 || ((m_passengers + addPasssengers) > Boen747Capacity)) ? 0 : addPasssengers;
+        return *this;
+    }
+
+    Flight& Flight::operator-=(double reduceFuel)
+    {
+        m_fuel -= (reduceFuel < 0 || (m_fuel - reduceFuel) < 0) ? 0 : reduceFuel;
+        return *this;
+    }
+
+    Flight& Flight::operator-=(int reducePasssengers)
+    {
+        m_passengers -= (reducePasssengers < 0 || (m_passengers - reducePasssengers) < 0) ? 0 : reducePasssengers;
+        return *this;
+    }
+
+    Flight& Flight::operator<<(Flight& transferFlight)
+    {
+        int sumPassengers = m_passengers + transferFlight.m_passengers;
+        m_passengers = ((Boen747Capacity - sumPassengers) < 0) ? Boen747Capacity : (Boen747Capacity - sumPassengers);
+        transferFlight.m_passengers = ((Boen747Capacity - sumPassengers) < 0) ? sumPassengers - Boen747Capacity : 0;
+        return *this;
+    }
+
+    Flight& Flight::operator>>(Flight& transferFlight)
+    {
+        int sumPassengers = m_passengers + transferFlight.m_passengers;
+        transferFlight.m_passengers = ((Boen747Capacity - sumPassengers) < 0) ? Boen747Capacity : (Boen747Capacity - sumPassengers);
+        m_passengers = ((Boen747Capacity - sumPassengers) < 0) ? sumPassengers - Boen747Capacity : 0;
+        return *this;
+    }
+
+
+    int Flight::operator+(const Flight& flight) const
+    {
+        return (~*this && ~flight) * (m_passengers + flight.m_passengers);
+    }
+
+    int operator+=(int& passengers, const Flight& flight)
+    {
+        passengers += int(flight);
+        return passengers;
     }
 
 }
--- WS05/lab/Flight.h
+++ WS05/lab/Flight.h
@@ -5,7 +5,14 @@
 // Version 1.0
 // Author   Asam Gulaid, revised by Fardad Soleimanloo
 // Description
+//			Name      : David Andres Sanchez Umbarila
+//			Student ID: 140273228
+//			Email     : dasanchez-umbarila@myseneca.ca
+/			Date      : 07/10/2023
 //
+//			I have done all the coding by myself and only copied the code
+//			 that my professor provided to complete my workshops and
+//			 assignments.
 // Revision History
 // -----------------------------------------------------------
 // Name                 Date            Reason
@@ -13,6 +20,8 @@
 #ifndef SDDS_FLIGHT_H
 #define SDDS_FLIGHT_H
 #include <iostream>
+
+#define LITER_PER_PERSON 600
 
 namespace sdds {
 
@@ -30,7 +39,24 @@ namespace sdds {
       Flight();
       Flight(int passengers, double fuel, const char* title);
       std::ostream& display()const;
+      operator bool() const;
+      operator int() const;
+      operator double() const;
+      operator const char*() const;
+      bool operator ~() const;
+      Flight& operator =(Flight& transferFlight);
+      Flight& operator =(int passengers);
+      Flight& operator =(double fuel);
+      Flight& operator +=(double addFuel);
+      Flight& operator +=(int addPasssengers);
+      Flight& operator -=(double reduceFuel);
+      Flight& operator -=(int reducePasssengers);
+      Flight& operator <<(Flight& transferFlight);
+      Flight& operator >>(Flight& transferFlight);
+      int operator +(const Flight& flight) const;
    };
+
+   int operator+=(int& passengers, const Flight& flight);
 
 }
 #endif // SDDS_FLIGHT_H
